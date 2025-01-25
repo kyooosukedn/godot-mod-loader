16d37be441dc45d3ce8a80afb37a30afd2ed3da1
Kai <kai@kana.jetzt>
2025-01-25 11:13:23 +0100

    refactor: :recycle: export props
    only exported properties are duplicated

--- addons/mod_loader/resources/mod_user_profile.gd
+++ addons/mod_loader/resources/mod_user_profile.gd
@@ -4,8 +4,8 @@ class_name ModUserProfile
 
 # This Class is used to represent a User Profile for the ModLoader.
 
-var name := ""
-var mod_list := {}
+export var name := ""
+export var mod_list := {}
 
 
 func _init(_name := "", _mod_list := {}) -> void:
